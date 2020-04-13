5d32a0fdedfbc0c5271055a6494266c0a32925b2
andrenevares <andre.nevares@gmail.com>
2020-04-13 19:07:15 -0300

    Update questao07.c

--- linguagemC/listasDeExercicios/lista002/questao07.c
+++ linguagemC/listasDeExercicios/lista002/questao07.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    float salario, indice, salarioReajustado;
+    
+    indice = 0.30;
+    printf("Programa de Cálculo de Reajuste de Salários\n\n");
+    
+    printf("Por favor insira o seu salário:..");
+    scanf("%f", &salario);
+    
+    if (salario >= 500.00) {
+        printf("Seu salário está acima do limite colocado para reajuste!\n");
+        printf("Seu salário continuará a ser de R$%.2f", salario);
+    }
+    else if (salario < 0){
+        printf("Salários não podem ter valor negativo!\n");
+        printf("Por favor, tente novamente");
+    }
+    else {
+        salarioReajustado = salario * (1.0 + indice);
+        printf("Seu salário de R$%.2f foi reajustado para R$%.2f", salario, salarioReajustado);
+    }
+}
