a3b1604cfb0df8ed591c1dc18bea984c04f62d92
andrenevares <andre.nevares@gmail.com>
2020-04-13 22:24:40 -0300

    Update questao17.c

--- linguagemC/listasDeExercicios/lista002/questao17.c
+++ linguagemC/listasDeExercicios/lista002/questao17.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+    
+    int senha;
+    
+    printf("Sistema de Login\n\n");
+    printf("Por favor digite sua senha:.. ");
+    scanf("%d", &senha);
+    
+    if (senha == 4531){
+        printf("Senha confirmada com sucesso!\n");
+        printf("Acesso permitido");
+    }
+    else {
+        printf("Acesso Negado");
+    }
+    
+    return 0;
+}   
