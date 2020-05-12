767b04ee508185074fd3bc8e6ac143fc157b2ce2
andrenevares <andre.nevares@gmail.com>
2020-05-12 08:51:55 -0300

    Create questao02.cpp

---
+++ linguagemC++/questao02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    float a, b, x;
+    
+    cout << "qual o valor de a? ";
+    cin >> a;
+    cout << "qual o valor de b? ";
+    cin >> b;
+    
+    x = (a + b * b) / 3 ;
+    
+    cout << x;
+    
+}
+    
