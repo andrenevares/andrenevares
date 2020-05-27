01065deb7e4327448c423962a57ef41a25a6662b
andrenevares <andre.nevares@gmail.com>
2020-05-27 20:37:14 -0300

    .

---
+++ python/scraping/oreileys/10.chapter02_scrape08.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+soup = BeautifulSoup(html, 'html.parser')
+
+images = soup.find_all('img', src=re.compile('.*.(jpg|png|svg)'))
+for image in images:
+    print(image['src'])
