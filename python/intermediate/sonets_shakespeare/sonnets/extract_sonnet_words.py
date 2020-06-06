93eeb5c37339126834ef9e6057d371d38a4fe206
andrenevares <andre.nevares@gmail.com>
2020-06-06 18:48:00 -0300

    .

--- python/intermediate/sonets_shakespeare/sonnets/extract_sonnet_words.py
+++ python/intermediate/sonets_shakespeare/sonnets/extract_sonnet_words.py
@@ -1,8 +1,13 @@
 import string
 
+# Open sonnets.txt and read all the lines
 sonnets = open("sonnets.txt").readlines()
+# for each elements in txt file remove white space, convert
+# into a set (Because set is so much faster then lists)
+# and this set is gonna be word_set
 word_set = set([elt.strip() for elt in open("sowpods.txt")])
 sonnet_words = set()
+
 
 def strip_punctuation(word):
     # Remove surrounding punctuation. If there's an apostrophe in the
@@ -12,6 +17,7 @@ def strip_punctuation(word):
     if apostrophe_index != -1:
         return None
     return word.strip(string.punctuation)
+
 
 for line in sonnets:
     # Split apart hyphenated words.
