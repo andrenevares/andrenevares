ad2be62ca3dfc46facf81a8a79032017de13db8c
andrenevares <andre.nevares@gmail.com>
2020-07-22 18:24:36 -0300

    .

---
+++ python/desafios/triple_print_solucao01.py
@@ -0,0 +1,4 @@
+def print_triplo(nome):
+    print (nome + nome + nome)
+
+print_triplo("Andre")
---
+++ python/desafios/triple_print_solucao02.py
@@ -0,0 +1,5 @@
+def print_triplo_dois(novo_nome):
+    return f"{novo_nome}"
+    
+frase = print_triplo_dois("André")
+print_triplo(frase)
